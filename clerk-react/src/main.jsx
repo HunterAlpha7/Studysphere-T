890760473c048532a092e1145e10cd652a1a82af
Seyam Rahman <175552400+HunterAlpha7@users.noreply.github.com>
2025-02-04 15:13:25 +0600

    Modular Nav bar

--- clerk-react/src/main.jsx
+++ clerk-react/src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
+import App from './App'
+import './index.css'
 
-
-// Import your Publishable Key
+// Get your Clerk publishable key from your Clerk Dashboard
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
@@ -14,7 +13,7 @@ if (!PUBLISHABLE_KEY) {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <App />
     </ClerkProvider>
   </React.StrictMode>,
